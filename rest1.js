077e402c3ab85baeac1506e6a0cf73e050d324d6
Shashank Sharma <shashank9226@gmail.com>
2024-10-19 11:42:02 +0530

    serving index.html

--- rest1.js
+++ rest1.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import { createServer } from 'http';
-import cors from 'cors';
+import express from "express";
+import { createServer } from "http";
 const app = express();
 const httpServer = createServer(app);
 
-app.get('/', (req, res) => {
-    res.cookie('auth_token', 'some long token value to be sent to cross domain', {
-        httpOnly: true,
-        expires: new Date(Date.now() + 60 * 60 * 1000),
-        sameSite: "none",
-        secure: "true"
-    });
-    res.sendFile(__dirname + 'index.html');
-})
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.cookie("auth_token", "some long token value to be sent to cross domain", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 60 * 1000),
+    sameSite: "none",
+    secure: "true",
+  });
+  res.sendFile("./public/index.html");
+});
 
 httpServer.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
